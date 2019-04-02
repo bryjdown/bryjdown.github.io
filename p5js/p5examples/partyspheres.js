bc96bf74a844684efe0a89a89685347f67c40711
Jay <bryjdown@ut.utm.edu>
2019-04-02 00:27:43 -0500

    Update partyspheres.js

--- p5js/p5examples/partyspheres.js
+++ p5js/p5examples/partyspheres.js
@@ -1,13 +1,35 @@
 let spheres = [];
+let xdetail = 16;
+let ydetail = 10;
 
 function setup(){
   var cnv = createCanvas(window.innerWidth, window.innerHeight, WEBGL);
   cnv.style('display', 'block');
   colorMode(HSB);
   angleMode(DEGREES);
-  spheres.push(new partySphere(100, 50, 1, 0, 0, true));
-  spheres.push(new partySphere(200, 50, -0.5, 0, 0, false));
-  spheres.push(new partySphere(300, 50, 0.25, 0, 0, false));
+  generateSpheres(9);
+}
+
+function generateSpheres(n){
+  let sz = 50;
+  let bfr = 0;
+  let lastbfr = 0;
+  let rotation = 1;
+
+  for(let i = 0; i < n; i++){
+    if(i == 0){
+      spheres.push(new partySphere(sz, 0, 0, 0, 0, true));
+    }
+    else{
+      spheres.push(new partySphere(sz, bfr, rotation, 0, 0, false));
+    }
+
+    bfr += 10;
+    sz += (20 * i) + 10;
+    rotation /= -1.5;
+
+    console.log(sz, " ", bfr);
+  }
 }
 
 function draw(){
@@ -20,6 +42,23 @@ function draw(){
   }
 }
 
+function keyPressed(){
+  if( keyCode == UP_ARROW && xdetail < 24 ){
+    xdetail++;
+  }
+  else if( keyCode == DOWN_ARROW && xdetail > 2){
+    xdetail--;
+  }
+  if( keyCode == RIGHT_ARROW && ydetail < 24 ){
+    ydetail++;
+  }
+  else if( keyCode == LEFT_ARROW && ydetail > 2){
+    ydetail--;
+  }
+
+  console.log(xdetail, " ", ydetail);
+}
+
 class partySphere{
   constructor(size, range, xRot, yRot, zRot, doFill){
     this.x = 0;
@@ -29,11 +68,13 @@ class partySphere{
     this.size = size;
     this.range = range;
     this.hue = 0;
-    this.sat = 180;
-    this.brt = 180;
+    this.sat = 100;
+    this.brt = 100;
     this.grow = true;
-    this.growSpeed = range / random(100, 150);
+    this.growSpeed = (0.001 * range) + (0.001 * log(size));
+    //Signifies CURRENT rotation.
     this.rotation = [xRot, yRot, zRot];
+    //Rotation speed.
     this.xRot = xRot;
     this.yRot = yRot;
     this.zRot = zRot;
@@ -42,29 +83,43 @@ class partySphere{
 
   show(){
     push();
+    //Rotating x, y, z.
     rotateX(this.rotation[0]);
     rotateY(this.rotation[1]);
     rotateZ(this.rotation[2]);
-    if(this.doFill){ fill(360 - this.hue, this.sat, this.brt) }
-    else{ noFill(); }
-    stroke(this.hue, this.sat, this.brt);
+    if(this.doFill){
+      fill(360, 0, 360);
+      stroke(360, 0, 360);
+    }
+    else{
+      noFill();
+      stroke(this.hue, this.sat, this.brt - (this.range));
+    }
     translate(0, 0, 0);
-    sphere(this.size);
+    sphere(this.size, xdetail, ydetail);
     pop();
   }
 
   update(){
-    this.hue = map(this.size, this.medSize - this.range, this.medSize + this.range, 0, 360);
+    let upperBound = this.medSize + this.range;
+    let lowerBound = this.medSize - this.range;
+
+    //Min size = 0 (red). Max size = 360 (red).
+    //NOTE: This "max hue" can be changed to alter the spectrum.
+    //      (180 = red -> cyan)
+    this.hue = map(this.size, lowerBound, upperBound, 0, 360);
     if(this.grow){ this.size += this.growSpeed; }
     else { this.size -= this.growSpeed; }
 
-    if(this.size <= this.medSize - this.range || this.size >= this.medSize + this.range ){
+    //Min size = start growing. Max size = start shrinking.
+    if(this.size <= lowerBound || this.size >= upperBound){
          this.grow = !this.grow;
-       }
-
+    }
+    //Apply rotation.
     this.rotation[0] += this.xRot;
     this.rotation[1] += this.yRot;
     this.rotation[2] += this.zRot;
+    //Preventing integer overflow, rotation should be bounded from (0-360).
     for(let axis of this.rotation){
       axis %= 360;
     }
