c89cb5c19aa026381069792c7d99bacd44e6cc66
Jay <bryjdown@ut.utm.edu>
2019-04-04 19:12:27 -0500

    Updated Rainbow Coil

--- p5js/p5examples/rainbow-coil.js
+++ p5js/p5examples/rainbow-coil.js
@@ -1,10 +1,16 @@
-let planets = []
+let SPH_DIST = 20;
+let SPH_SIZE = 4;
+let NUM_PLANETS = 24;
+let planets = [];
 
 function setup(){
   var cnv = createCanvas(window.innerWidth, window.innerHeight, WEBGL);
   cnv.style('display', 'block');
-  for(let i = 0; i < 50; i++){
-    planets.push(new planet(i * 10, 0, 10, 400 - 5*i, i));
+  for(let i = 0; i < NUM_PLANETS; i++){
+    //The 300 + i portion of this can be changed for different patterns.
+    //ex. 300 + (i % 2) * 50
+    //ex. 300 - (i % 5) * 20
+    planets.push(new planet(i * SPH_DIST, 0, SPH_SIZE, 250 + i, i));
   }
   frameRate(30);
   colorMode(HSB);
@@ -24,43 +30,52 @@ class planet{
   constructor(x, y, size, spd, index){
     this.x = x;
     this.y = y;
-    this.z = 0; //spd < 400 ? random(-100, 100) : 0;
+    this.z = 0;
     this.size = size;
     this.cDist = x;
-    this.oRotation = 0; //random(TWO_PI);
+    this.oRotation = 0;
     this.framesPerOrbit = spd;
-    this.color = this.x % 360;
+    this.color = map(this.x, 0, NUM_PLANETS*SPH_DIST, 0, 340);
     this.sat = 100;
     this.brt = 100;
     this.index = index;
-    this.x2 = index != 0 ? planets[index-1].x : 0;
-    this.y2 = index != 0 ? planets[index-1].y : 0;
+    this.lines = []
   }
 
   show(){
     push();
-    fill(this.color, this.sat, this.brt);
+    noFill();
     stroke(this.color, this.sat, this.brt);
-    //line(this.x, this.y, 0, this.x2, this.y2, 0);
-    line(0, 0, 0, this.x, this.y, 0);
+    //Drawing the tail of each sphere.
+    beginShape();
+    for(let ln of this.lines){
+      vertex(ln[0], ln[1], 0);
+    }
+    endShape();
+    fill(this.color, this.sat, this.brt);
     translate(this.x, this.y, this.z);
     sphere(this.size);
     pop();
   }
 
   update(){
-    this.x2 = this.index != 0 ? planets[this.index-1].x : 0;
-    this.y2 = this.index != 0 ? planets[this.index-1].y : 0;
-
+    //The center sphere shouldn't move.
     if(this.framesPerOrbit != 0){
-      this.x = this.cDist * cos(this.oRotation);
-      this.y = this.cDist * sin(this.oRotation);
+      this.x = this.cDist * Math.cos(this.oRotation);
+      this.y = this.cDist * Math.sin(this.oRotation);
     }
     else{
       this.x = 0;
       this.y = 0;
     }
+    //Rotation is from 0-2pi.
     this.oRotation += (TWO_PI / this.framesPerOrbit);
     this.oRotation %= (TWO_PI);
+
+    if(this.lines.length > 100){
+      this.lines.shift();
+    }
+    //Potential huge memory sink here, however only 2 floats are stored.
+    this.lines.push([this.x, this.y]);
   }
 }
