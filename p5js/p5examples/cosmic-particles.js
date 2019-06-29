314fe69515a087abe6a4febe8122146226e7f691
Jay <bryjdown@ut.utm.edu>
2019-06-28 23:17:20 -0500

    Updated Cosmic Particles

--- p5js/p5examples/cosmic-particles.js
+++ p5js/p5examples/cosmic-particles.js
@@ -1,10 +1,18 @@
 let part;
 let pressed = false;
+let attractors = [];
+let repulsors = [];
+let currentForceNodes = 0;
+let displayHelpText = true;
+let mouseAttractEnabled = false;
+let mouseRepelEnabled = false;
 
 let NUM_PARTICLES = 255;
 let MAX_SPEED = 5;
-let MIN_ACC_MULT = 0.0005;
-let MAX_ACC_MULT = 0.001;
+let MIN_ACC_MULT = 0.000005; 
+let MAX_ACC_MULT = 0.0005; 
+let DRAG_COEFF = 0.99;
+let MAX_FORCE_NODES = 12;
 
 function setup() {
   var cnv = createCanvas(window.innerWidth, window.innerHeight);
@@ -20,21 +28,60 @@ function setup() {
 
 function draw() {
   background(0, 0, 0, 0.05);
+  for(let attractor of attractors){
+    attractor.show();
+  }
+  for(let repulsor of repulsors){
+    repulsor.show();
+  }
   part.update();
   part.show();
-
+  if(displayHelpText){
+    push();
+    textSize(14);
+    fill(255);
+    stroke(0);
+    text("Click to spawn an attractor. Shift-click to spawn a repulsor.", 20, 20);
+    text("Press SPACE to clear attractors and repulsors.", 20, 40);
+    text("Press A or R to toggle attracting or repelling with the mouse.", 20, 60);
+    text("Press H to hide this text.", 20, 80);
+    pop();
+  }
 }
 
 function windowResized() {
    resizeCanvas(window.innerWidth, window.innerHeight);
+   background(0);
 }
 
 function mousePressed(){
-  pressed = true;
+  if(currentForceNodes < MAX_FORCE_NODES){
+    if(keyIsDown(SHIFT)){
+      repulsors.push(new ForceNode(mouseX, mouseY, 15, true));
+    }
+    else{
+      attractors.push(new ForceNode(mouseX, mouseY, 15, false));
+    }
+    currentForceNodes++;
+  }
 }
 
-function mouseReleased(){
-  pressed = false;
+function keyPressed(){
+  if(keyCode == 32){
+    repulsors = [];
+    attractors = [];
+  }
+  if(keyCode == 72){
+    displayHelpText = false;
+  }
+  if(keyCode == 65){
+    mouseAttractEnabled = !mouseAttractEnabled;
+    mouseRepelEnabled = false;
+  }
+  if(keyCode == 82){
+    mouseRepelEnabled = !mouseRepelEnabled;
+    mouseAttractEnabled = false;
+  }
 }
 
 class ParticleSystem {
@@ -51,11 +98,19 @@ class ParticleSystem {
 
   update(){
     let mpos = createVector(mouseX, mouseY);
-    if(!pressed){
-      this.moveTowards(mpos);
+     if(mouseAttractEnabled){
+       this.moveTowards(mpos);
+     }
+     else if(mouseRepelEnabled){
+       this.moveAway(mpos);
+     }
+    for(let attractor of attractors){
+      var apos = createVector(attractor.x, attractor.y);
+      this.moveTowards(apos);
     }
-    else{
-      this.moveAway(mpos);
+    for(let repulsor of repulsors){
+      var rpos = createVector(repulsor.x, repulsor.y);
+      this.moveAway(rpos);
     }
     for(let particle of this.particles){
       particle.update();
@@ -92,18 +147,15 @@ class Particle {
     this.vel.add(this.acc);
     this.pos.add(this.vel);
 
-    this.acc.mult(0.99);
-    this.vel.mult(0.99);
+    this.acc.mult(DRAG_COEFF);
+    this.vel.mult(DRAG_COEFF);
 
-    this.pos.x = constrain(this.pos.x, 0, width);
-    this.pos.y = constrain(this.pos.y, 0, height);
     this.acc.limit(MAX_SPEED);
     this.vel.limit(MAX_SPEED);
   }
 
   show(){
     push();
-    //var col = map(this.pos.x, 0, width, 0, 360);
     var col = map(this.vel.mag(), 0, MAX_SPEED, 0, 360, true);
 
     stroke(col, 100, 100);
@@ -115,7 +167,7 @@ class Particle {
   moveTowards(target){
     var force = p5.Vector.sub(target, this.pos).normalize();
 
-    var multi = map(p5.Vector.dist(this.pos, target), 0, width, MIN_ACC_MULT, MAX_ACC_MULT);
+    var multi = map(p5.Vector.dist(this.pos, target), 0, height, MAX_ACC_MULT, MIN_ACC_MULT, true);
     force.mult(multi);
     this.addForce(force);
   }
@@ -123,7 +175,7 @@ class Particle {
   moveAway(target){
     var force = p5.Vector.sub(this.pos, target).normalize();
 
-    var multi = map(p5.Vector.dist(this.pos, target), 0, width, MAX_ACC_MULT, MIN_ACC_MULT);
+    var multi = map(p5.Vector.dist(this.pos, target), 0, height, MAX_ACC_MULT, MIN_ACC_MULT, true);
     force.mult(multi);
     this.addForce(force);
   }
@@ -132,4 +184,21 @@ class Particle {
     this.acc.add(vector);
   }
 
+}
+
+class ForceNode{
+  constructor(x, y, r, isrepulsor){
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.isrepulsor = isrepulsor;
+  }
+
+  show(){
+    push();
+    noFill();
+    stroke(this.isrepulsor ? color('lightcoral') : color('springgreen'));
+    ellipse(this.x, this.y, this.r, this.r);
+    pop();
+  }
 }
